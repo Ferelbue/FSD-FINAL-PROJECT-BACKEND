79e0492eb2ef2f18b92356a62547c4999a878f17
Fernando <ferelbue@gmail.com>
2024-05-09 00:16:53 +0200

    feat: get all messages endpoint added

--- src/routes/chats.routes.ts
+++ src/routes/chats.routes.ts
@@ -1,13 +1,14 @@
 
 import { Router } from "express";
 import { auth } from "../middlewares/auth";
-import { BringAllMessages, createMessage, deleteMessage, eraseNotification, getMessages, getUserChats, notification } from "../controllers/chatsController";
+import { BringAllMessages, BringAllMessagesNumber, createMessage, deleteMessage, eraseNotification, getMessages, getUserChats, notification } from "../controllers/chatsController";
 import { isAdmin } from "../middlewares/isAdmin";
 
 const router = Router()
 
 router.get('/', auth, getUserChats)
 router.get('/all', auth, isAdmin, BringAllMessages)
+router.get('/allNumber', auth, isAdmin, BringAllMessagesNumber)
 router.get('/:productId/:userUserId', auth, getMessages)
 router.post('/:productId/:userUserId', auth, createMessage)
 router.delete('/:id', auth, deleteMessage)
